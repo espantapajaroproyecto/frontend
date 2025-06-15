e838be56b5476cd3fe151777a257321599b5b5e5
Mauro Palgi <maurolink@gmail.com>
2025-06-15 00:01:27 -0300

    fix: :bug: PI-97 | Update to new api gateway, Update login axios body
    https://proyectoespanta.atlassian.net/browse/PI-97

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -3,14 +3,11 @@ import axios from "axios";
 export async function login(dni, password) {
   try {
     console.log(process.env.API_BASE_URL);
-    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}`, {
-      body: {
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}login`, {
+      body: JSON.stringify({
         dni: dni,
-        password: password
-      },
-      headers: {
-        "Content-Type": "application/json"
-      }
+        contrasenia: password
+      })
     });
     console.log(response);
     return response.data;
